e6cf0d50939266904115f93c0f5487c4a6fcd0ef
Egor <meekgot@gmail.com>
2020-08-05 23:52:59 +0300

    feat. загрузил все картинки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import './scss/app.scss';
+
+
+/**
+ * Logo
+ */
+import './assets/images/logo.png';
+
+
+/**
+ * Icons
+ */
+import './assets/images/figma-icon.png';
+import './assets/images/mokup-icon.png';
+import './assets/images/indesign-icon.png';
+import './assets/images/ae-icon.png';
+import './assets/images/ai-icon.png';
+import './assets/images/dn-icon.png';
+import './assets/images/ps-icon.png';
+import './assets/images/xd-icon.png';
+
+
+/**
+ * Background
+ */
+import './assets/images/hero-bg.jpg';
+
+
+/**
+ * Illustrations
+ */
+import './assets/images/illustration01.png';
+import './assets/images/illustration02.png';
+import './assets/images/illustration03.png';
+
+
+/**
+ * Exp
+ */
+import './assets/images/work01-image.png';
+import './assets/images/work02-image.png';
+import './assets/images/work03-image.png';
+import './assets/images/work04-image.png';
+
+
+/**
+ * Learn
+ */
+import './assets/images/learn01-image.png';
+import './assets/images/learn02-image.png';
+import './assets/images/learn03-image.png';
+
+/**
+ * Messages 
+ */
+import './assets/images/messages-icon01.png'
+import './assets/images/dizain-takoi.png'
