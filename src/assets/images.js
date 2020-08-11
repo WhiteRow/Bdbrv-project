2e1d414bcac3809815c52aa1eeeaa47ed2535905
Egor <meekgot@gmail.com>
2020-08-11 23:51:23 +0300

    feat.написал базовые компоненты

---
+++ src/assets/images.js
@@ -0,0 +1,54 @@
+/**
+ * Logo
+ */
+import './images/logo.png';
+
+
+/**
+ * Icons
+ */
+import './images/figma-icon.png';
+import './images/mokup-icon.png';
+import './images/indesign-icon.png';
+import './images/ae-icon.png';
+import './images/ai-icon.png';
+import './images/dn-icon.png';
+import './images/ps-icon.png';
+import './images/xd-icon.png';
+
+
+/**
+ * Background
+ */
+import './images/hero-bg.jpg';
+
+
+/**
+ * Illustrations
+ */
+import './images/illustration01.png';
+import './images/illustration02.png';
+import './images/illustration03.png';
+
+
+/**
+ * Exp
+ */
+import './images/work01-image.png';
+import './images/work02-image.png';
+import './images/work03-image.png';
+import './images/work04-image.png';
+
+
+/**
+ * Learn
+ */
+import './images/learn01-image.png';
+import './images/learn02-image.png';
+import './images/learn03-image.png';
+
+/**
+ * Messages 
+ */
+import './images/messages-icon01.png'
+import './images/dizain-takoi.png'
